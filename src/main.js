97b6f0379864b24405d68b35a80ec63d5cfc745c
Neeraj Yadav <er.neerajyadav@gmail.com>
2018-04-07 20:25:06 +0530

    initial resolve with inefficient version. optimized after reading hint.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,14 @@
 for (let num = 0; num < 50;) {
     console.log((++num % 3 ? "" : "fuzz") + (num % 5 ? "" : "buzz") || num);
 }
+
+//Chess-board-problem
+const size = 8;
+let str = "";
+for (let len = 0; len < size; len++) {
+    for (let wid = 0; wid < size; wid++) {
+        str += (len + wid) % 2 ? " " : "#";
+    }
+    str += "\n";
+}
+console.log(str);
