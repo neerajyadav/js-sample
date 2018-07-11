1b123203eb6126e4ad68dca0fe55258628a26ff9
Neeraj Kumar <er.neerajyadav@gmail.com>
2018-07-11 15:00:47 +0530

    testing git commit without stage feature

--- src/main.js
+++ src/main.js
@@ -13,3 +13,53 @@ for (let len = 0; len < size; len++) {
     str += "\n";
 }
 console.log(str);
+
+function range(start, end, step = 1) {
+    let arr = [];
+    if (start <= end) {
+        for (let i = start; i <= end; i = i + step) {
+            arr.push(i);
+        }
+    }
+    else {
+        step = step === 1 ? -1 : step;
+        for (let i = start; i >= end; i = i + step) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+function reverseArray(arr){
+    let newArr = []
+    for(let a of arr){
+        newArr.unshift(a);
+    }
+    return newArr;
+}
+
+function reverseArrayInPlace(arr){
+    for(let a of arr){
+        arr.push(a);
+        arr.shift();
+    }
+}
+
+function deepEqual(obj1, obj2){
+    if(obj1 != undefined && typeof obj1 == "object" && obj2 != undefined && typeof obj2 == "object"){
+        let keys1 = Object.keys(obj1);
+        for(let prop of keys1){
+            if(obj2[prop]!= undefined && obj2[prop] == obj1[prop]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;    
+    }
+    else{
+        return obj1 == obj2;
+    }
+}
+
